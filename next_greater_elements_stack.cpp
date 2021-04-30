e2e5a9d7db87ab035585983217c0deeb48141f82
Sagar Rawat <sagarrawat089@gmail.com>
2021-04-30 18:11:46 +0530

    Luv_STL_PALYLIST

---
+++ next_greater_elements_stack.cpp
@@ -0,0 +1,58 @@
+// Next Greater Elements  --Luv Playlist STL
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> NGE(vector<int> v){
+    vector<int> nge(v.size());
+    stack<int> st;
+    for(int i=0; i<v.size();i++)
+    {
+        while(!st.empty() && v[i] > v[st.top()])
+        {
+            nge[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty())
+    {
+        nge[st.top()] = -1;
+        st.pop();
+    }
+    return nge;
+}
+int main(){
+   
+    int n;
+    cout<<"N :"<<endl;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n;i++)
+    {
+        cin>>v[i];
+    }
+
+    vector<int> nge = NGE(v);
+    cout<<"Output"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cout<< v[i] <<" " << (nge[i] == -1 ? -1 : v[nge[i]])<< endl;
+    }
+    return 0;
+}
+
+
+// Output
+// N :
+// 5
+// 2
+// 4
+// 2
+// 5
+// 67
+// Output
+// 2 4
+// 4 5
+// 2 5
+// 5 67
+// 67 -1
